dc653eed66f8c098ee80ad81264ee43c8130cbe2
Adam Becvar <becvar.adam666@gmail.com>
2017-05-21 19:00:33 +0200

    adds vertical dimension notes to side frame blueprint fix font rendering

--- src/main/java/org/cba/model/carport/formating/svg/SVGGenerator.java
+++ src/main/java/org/cba/model/carport/formating/svg/SVGGenerator.java
@@ -1,6 +1,7 @@
 package org.cba.model.carport.formating.svg;
 
 import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.svggen.SVGGeneratorContext;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.svggen.SVGGraphics2DIOException;
 import org.cba.domain.Frame;
@@ -9,6 +10,7 @@ import org.jetbrains.annotations.NotNull;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 
+import java.awt.*;
 import java.io.StringWriter;
 import java.io.Writer;
 
@@ -19,14 +21,23 @@ public class SVGGenerator {
 
     public String generate(BlueprintDrawing blueprintDrawing) throws SVGGraphics2DIOException {
         DOMImplementation domImpl = GenericDOMImplementation.getDOMImplementation();
-        Document document = domImpl.createDocument("", "svg", null);
-        SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+        String svgNS = "http://www.w3.org/2000/svg";
+        Document document = domImpl.createDocument(svgNS, "svg", null);
+        SVGGeneratorContext ctx = SVGGeneratorContext.createDefault(document);
+        ctx.setEmbeddedFontsOn(true);
+        SVGGraphics2D svgGenerator = new SVGGraphics2D(ctx, true);
+        setStrokeAndFont(svgGenerator);
         blueprintDrawing.draw(svgGenerator);
 
         Writer out = new StringWriter();
         boolean useCss = true;
         svgGenerator.stream(out, useCss);
         return removeNonSVGTags(out);
+    }
+
+    private void setStrokeAndFont(SVGGraphics2D svgGenerator) {
+        svgGenerator.setStroke(new BasicStroke(2));
+        svgGenerator.setFont(new Font("Verdana", Font.BOLD, 20));
     }
 
     @NotNull
